cc035ee2eb68bdb0968d1505ab73bc49716312ef
Evan <503324687@qq.com>
2023-09-10 12:12:15 +0800

    first commit

---
+++ src/main/java/com/capstone/shri/ShriApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.shri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/shri/ShriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.shri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
